e6a3a6c595f67530a3638f00587b049a7589a21d
Nof Shabtay <nofsh.sp@gmail.com>
2022-10-27 22:33:08 +0300

    .

--- server/server.py
+++ server/server.py
@@ -24,4 +24,24 @@ def parsePort(file):
     finally:
         return port
     
+class Server:
+    DB = 'server.db'
+    PACKET_SIZE = 1024   # Default packet size.
+    MAX_QUEUED_CONN = 5  # Default maximum number of queued connections.
+    IS_BLOCKING = False  # Do not block!
 
+    def __init__(self, host, port):
+        """ Initialize server. Map request codes to handles. """
+        logging.basicConfig(format='[%(level)s -- %(time)s] -- %(note)s', level=logging.INFO, datefmt='%H:%M:%S')
+        self.host = host
+        self.port = port
+        self.sel = selectors.DefaultSelector()
+        self.db = DB.Database(Server.DB)
+        self.err = ""  # error description.
+        self.requests = {
+            protocol.ERequestCode.REQUEST_REGISTRATION.value: self.handleRegistrationRequest,
+            protocol.ERequestCode.REQUEST_USERS.value: self.handleUsersListRequest,
+            protocol.ERequestCode.REQUEST_PUBLIC_KEY.value: self.handlePublicKeyRequest,
+            protocol.ERequestCode.REQUEST_SEND_MSG.value: self.handleMessageSendRequest,
+            protocol.ERequestCode.REQUEST_PENDING_MSG.value: self.handlePendingMessagesRequest
+        }
